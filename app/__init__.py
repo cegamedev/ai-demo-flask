20aa4932c26d8b75d850a030f52b2b7dab918d17
朱新辉 <zhuxinhui@imzhiliao.com>
2017-03-31 18:29:00 +0800

    链接tensorflow serving成功

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.STATIC_URL = 'static'
 
 import biz_ctrl
--- app/biz_ctrl.py
+++ app/biz_ctrl.py
@@ -1,10 +1,25 @@
 # coding=utf-8
 
-from flask import request
-from . import app
+from flask import request, redirect
+from . import app, mnist_client
 import json
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return redirect('/static/index.html')
+
+
+@app.route('/static')
+def static_entry():
+    return redirect('/static/index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'error 404', 404
+
+
+@app.route('/mnist')
+def mnist():
+    return mnist_client.main(1)
