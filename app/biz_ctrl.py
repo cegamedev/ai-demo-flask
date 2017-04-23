a62af2d9a75067c9a6147568e26ac15c52fddc94
zhuxinhui <zhuxinhui@imzhiliao.com>
2017-04-24 07:47:32 +0800

    预处理

--- app/biz_ctrl.py
+++ app/biz_ctrl.py
@@ -4,7 +4,8 @@ from flask import request, redirect
 from . import app, square_client, mnist_softmax_client, mnist_input_data
 import json
 import cv2
-from PIL import Image
+import cv2.cv as cv
+from PIL import Image, ImageFilter
 import numpy as np
 
 '''
@@ -51,17 +52,39 @@ def api_calculator():
 def upload_image():
     file = request.files['file']
     size = (28, 28)
-    im = Image.open(file)
-    im.thumbnail(size)
-    # im.save('app/static/img/test.png')
-    # 转灰度
-    gray_im = im.convert('L')
-    # im.save('app/static/img/test_auto.png')
-    # 转数组，并由二维转一维
+    # img = Image.open(file)
+    # img = img.filter(ImageFilter.CONTOUR)
+    # img.thumbnail(size)
+    # img.save('app/static/img/test.png')
+    # # 转灰度
+    # gray_im = img.convert('L')
+    # gray_im.save('app/static/img/test_auto.png')
+    # # 转数组，并由二维转一维
 
-    gray_im = Image.open('app/static/img/test_cv.png')
+    # # gray_im = Image.open('app/static/img/test_cv.png')
 
-    gray_im_arr = np.array(gray_im).reshape(784) / 255.0
+    img = cv2.imread('app/static/img/mnist/images/10_10.png', 0)
+    img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
+    # img = img.convert('F')
+    cv2.imwrite("app/static/img/test.png", img)
+    # img = cv2.blur(img, (3, 3))
+    # img = cv2.GaussianBlur(img, (3, 3), 0)
+    # img = cv2.bilateralFilter(img, 21, 21, 21)
+    # img = cv2.medianBlur(img, 9)
+
+    # cv2.imwrite("app/static/img/test_auto.png", img)
+
+    # img = cv2.adaptiveThreshold(
+    #     img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 3)
+    # cv2.imwrite("app/static/img/test_auto2.png", img)
+
+    # img = Image.open('app/static/img/test_m_1.png')
+    # img = img.convert('F')
+
+    # img = cv2.equalizeHist(img)
+    # cv2.imwrite("app/static/img/test_auto2.png", img)
+
+    gray_im_arr = np.array(img).reshape(784)
     result = {}
     result['data'] = mnist_softmax_client.main(gray_im_arr)
     soft_arr = result['data']['tensor']['data']
